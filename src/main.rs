e012f8ba1c17f003411c7f83ca68190f6557fa4d
Quentin Santos <qsantos@qsantos.fr>
2023-05-26 09:18:13 +0200

    Parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,195 @@
-use std::io;
+use std::{
+    fmt::Display,
+    io::{self, BufRead, Lines},
+    num::ParseIntError,
+    str::FromStr,
+};
 
-macro_rules! parse_input {
-    ($x:expr, $t:ident) => {
-        $x.trim().parse::<$t>().unwrap()
-    };
+#[derive(Debug)]
+enum ParsingError {
+    WrongNumberOfElements(String, usize, usize),
+    NotAnInteger(String, ParseIntError),
+    InvalidCellKind(i32),
+    EarlyEndOfFile,
+    IoError(io::Error),
 }
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-fn main() {
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    let number_of_cells = parse_input!(input_line, i32); // amount of hexagonal cells in this map
-    for i in 0..number_of_cells as usize {
-        let mut input_line = String::new();
-        io::stdin().read_line(&mut input_line).unwrap();
-        let inputs = input_line.split(" ").collect::<Vec<_>>();
-        let _type = parse_input!(inputs[0], i32); // 0 for empty, 1 for eggs, 2 for crystal
-        let initial_resources = parse_input!(inputs[1], i32); // the initial amount of eggs/crystals on this cell
-        let neigh_0 = parse_input!(inputs[2], i32); // the index of the neighbouring cell for each direction
-        let neigh_1 = parse_input!(inputs[3], i32);
-        let neigh_2 = parse_input!(inputs[4], i32);
-        let neigh_3 = parse_input!(inputs[5], i32);
-        let neigh_4 = parse_input!(inputs[6], i32);
-        let neigh_5 = parse_input!(inputs[7], i32);
+fn parse_i32(s: &str) -> Result<i32, ParsingError> {
+    s.trim()
+        .parse::<i32>()
+        .map_err(|e| ParsingError::NotAnInteger(s.to_string(), e))
+}
+
+fn parse_usize(s: &str) -> Result<usize, ParsingError> {
+    s.trim()
+        .parse::<usize>()
+        .map_err(|e| ParsingError::NotAnInteger(s.to_string(), e))
+}
+
+fn next_line<T: BufRead>(lines: &mut Lines<T>) -> Result<String, ParsingError> {
+    match lines.next() {
+        None => Err(ParsingError::EarlyEndOfFile),
+        Some(Err(e)) => Err(ParsingError::IoError(e)),
+        Some(Ok(v)) => Ok(v),
     }
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    let number_of_bases = parse_input!(input_line, i32);
-    let mut inputs = String::new();
-    io::stdin().read_line(&mut inputs).unwrap();
-    for i in inputs.split_whitespace() {
-        let my_base_index = parse_input!(i, i32);
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum CellKind {
+    Empty = 0,
+    Eggs = 1,
+    Crystals = 2,
+}
+
+impl TryFrom<i32> for CellKind {
+    type Error = ParsingError;
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        match value {
+            x if x == CellKind::Empty as i32 => Ok(CellKind::Empty),
+            x if x == CellKind::Eggs as i32 => Ok(CellKind::Eggs),
+            x if x == CellKind::Crystals as i32 => Ok(CellKind::Crystals),
+            _ => Err(ParsingError::InvalidCellKind(value)),
+        }
     }
-    let mut inputs = String::new();
-    io::stdin().read_line(&mut inputs).unwrap();
-    for i in inputs.split_whitespace() {
-        let opp_base_index = parse_input!(i, i32);
+}
+
+#[derive(Clone, Debug)]
+struct Cell {
+    kind: CellKind,
+    resources: i32,
+    _neigh_1: i32,
+    _neigh_2: i32,
+    _neigh_3: i32,
+    _neigh_4: i32,
+    _neigh_5: i32,
+    _neigh_6: i32,
+    allied_ants: i32,
+    ennemy_ants: i32,
+}
+
+impl FromStr for Cell {
+    type Err = ParsingError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let inputs: Vec<i32> = s
+            .split(" ")
+            .map(|s| parse_i32(s))
+            .collect::<Result<Vec<i32>, _>>()?;
+        if inputs.len() != 8 {
+            return Err(ParsingError::WrongNumberOfElements(
+                s.to_string(),
+                inputs.len(),
+                8,
+            ));
+        }
+        let base_cell = Cell {
+            kind: inputs[0].try_into()?,
+            resources: inputs[1],
+            _neigh_1: inputs[2],
+            _neigh_2: inputs[3],
+            _neigh_3: inputs[4],
+            _neigh_4: inputs[5],
+            _neigh_5: inputs[6],
+            _neigh_6: inputs[7],
+            allied_ants: 0,
+            ennemy_ants: 0,
+        };
+        Ok(base_cell)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Game {
+    cells: Vec<Cell>,
+    allied_bases: Vec<usize>,
+    ennemy_bases: Vec<usize>,
+}
+
+impl Game {
+    fn parse_bases(line: &str, count: usize) -> Result<Vec<usize>, ParsingError> {
+        let ret = line
+            .split(' ')
+            .map(|s| parse_usize(s))
+            .collect::<Result<Vec<usize>, _>>()?;
+        if ret.len() != count {
+            return Err(ParsingError::WrongNumberOfElements(
+                line.to_string(),
+                ret.len(),
+                count,
+            ));
+        }
+        Ok(ret)
     }
 
-    // game loop
-    loop {
-        for i in 0..number_of_cells as usize {
-            let mut input_line = String::new();
-            io::stdin().read_line(&mut input_line).unwrap();
-            let inputs = input_line.split(" ").collect::<Vec<_>>();
-            let resources = parse_input!(inputs[0], i32); // the current amount of eggs/crystals on this cell
-            let my_ants = parse_input!(inputs[1], i32); // the amount of your ants on this cell
-            let opp_ants = parse_input!(inputs[2], i32); // the amount of opponent ants on this cell
+    fn parse<T: BufRead>(lines: &mut Lines<T>) -> Result<Game, ParsingError> {
+        let number_of_cells = parse_usize(&next_line(lines)?)?;
+        let mut cells = Vec::new();
+        for _ in 0..number_of_cells {
+            cells.push(next_line(lines)?.parse()?);
         }
 
-        // Write an action using println!("message...");
-        // To debug: eprintln!("Debug message...");
+        let number_of_bases = parse_usize(&next_line(lines)?)?;
+        let allied_bases = Game::parse_bases(&next_line(lines)?, number_of_bases)?;
+        let ennemy_bases = Game::parse_bases(&next_line(lines)?, number_of_bases)?;
 
-        // WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
-        println!("WAIT");
+        Ok(Game {
+            cells,
+            allied_bases,
+            ennemy_bases,
+        })
+    }
+}
+
+struct ActionLine {
+    source: usize,
+    destination: usize,
+    strength: i32,
+}
+
+struct ActionBeacon {
+    location: usize,
+    strength: i32,
+}
+
+struct ActionMessage {
+    message: String,
+}
+
+enum Action {
+    Wait,
+    Line(ActionLine),
+    Beacon(ActionBeacon),
+    ActionMessage(ActionMessage),
+}
+
+impl Display for Action {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Action::Wait => write!(f, "WAIT"),
+            Action::Line(line) => write!(
+                f,
+                "LINE {} {} {}",
+                line.source, line.destination, line.strength
+            ),
+            Action::Beacon(beacon) => write!(f, "BEACON {} {}", beacon.location, beacon.strength),
+            Action::ActionMessage(message) => write!(f, "MESSAGE {}", message.message),
+        }
+    }
+}
+
+fn main() {
+    let mut lines = io::stdin().lines();
+    let mut game = Game::parse(&mut lines).unwrap();
+
+    loop {
+        for cell in game.cells.iter_mut() {
+            let line = next_line(&mut lines).unwrap();
+            let inputs = line.split(" ").collect::<Vec<_>>();
+            cell.resources = parse_i32(inputs[0]).unwrap();
+            cell.allied_ants = parse_i32(inputs[1]).unwrap();
+            cell.ennemy_ants = parse_i32(inputs[2]).unwrap();
+        }
+
+        let action = Action::Wait;
+        println!("{}", action);
     }
 }
