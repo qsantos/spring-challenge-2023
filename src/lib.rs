286ccbe98a2b84b6c33c38f1948c4659d0676fae
Quentin Santos <qsantos@qsantos.fr>
2023-05-30 20:17:56 +0200

    Remove obsolete main function from lib module

--- src/lib.rs
+++ src/lib.rs
@@ -391,39 +391,3 @@ impl Display for Action {
         }
     }
 }
-
-fn main() {
-    let mut game = Game::parse().unwrap();
-
-    loop {
-        game = game.read_update().unwrap();
-
-        let mut action = Action::Wait;
-
-        let allied_base = game.allied_bases[0];
-        let closest_eggs = game.closest_cell(allied_base, CellKind::Eggs);
-        let closest_crystals = game.closest_cell(allied_base, CellKind::Crystals);
-
-        if let Some((distance, index)) = closest_eggs {
-            if distance < 5 {
-                action = Action::Line(ActionLine {
-                    source: allied_base,
-                    destination: index,
-                    strength: 100,
-                });
-            }
-        }
-
-        if action == Action::Wait {
-            if let Some((distance, index)) = closest_crystals {
-                action = Action::Line(ActionLine {
-                    source: allied_base,
-                    destination: index,
-                    strength: 100,
-                });
-            }
-        }
-
-        println!("{}", action);
-    }
-}
