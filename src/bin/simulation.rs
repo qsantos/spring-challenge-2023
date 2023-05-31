1c84ece7a438f9522ffaf13cc4517d9fa554ba6d
Quentin Santos <qsantos@qsantos.fr>
2023-05-31 21:53:59 +0200

    Start bot processes

--- src/bin/simulation.rs
+++ src/bin/simulation.rs
@@ -5,11 +5,16 @@ use spring_challenge_2023::Game;
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     if args.len() != 3 {
-        println!("!");
+        println!("Usage: {} bot1 bot2", args[0]);
         return;
     }
-    let bot1 = &args[1];
-    let bot2 = &args[2];
+    let bot1 = Command::new(&args[1])
+        .spawn()
+        .expect("Failed to start first bot");
+
+    let bot2 = Command::new(&args[2])
+        .spawn()
+        .expect("Failed to start first bot");
 
     let game = Game::parse().unwrap();
     game.write();
