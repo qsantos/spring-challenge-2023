c1fd661a84630ab529dd0a5b65f9fea4f5e4da4e
Quentin Santos <qsantos@qsantos.fr>
2023-05-31 21:50:42 +0200

    Load bot programs from command-line arguments

--- src/bin/simulation.rs
+++ src/bin/simulation.rs
@@ -1,6 +1,16 @@
+use std::process::Command;
+
 use spring_challenge_2023::Game;
 
 fn main() {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 3 {
+        println!("!");
+        return;
+    }
+    let bot1 = &args[1];
+    let bot2 = &args[2];
+
     let game = Game::parse().unwrap();
     game.write();
 }
